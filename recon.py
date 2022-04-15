a25b60dc29efa9185eeec44d559a084b1f143276
htevil <htsmart42@gmail.com>
2022-04-16 00:15:44 +0530

    Update recon.py

--- recon.py
+++ recon.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 url = input('Enter the url of web site :')
 #url = 'https://www.amazon.in/'
+lis = []
 
 class recon:
     def getall_link (self,url):
@@ -12,7 +13,10 @@ class recon:
         page= requests.get(url)
         soup = BeautifulSoup (page.content, 'html.parser')
         #print(soup.prettify)
-        print( soup.find_all('herf'))
+        #print( soup.find_all('a', href = True))
+        for link in  soup.find_all('a', href = True):
+            lis.append(link['href'])
+        print(lis)
 
 r = recon()
 r.getall_link(url)
